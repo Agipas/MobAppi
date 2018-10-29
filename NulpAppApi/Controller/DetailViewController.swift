289baac608f08c1e9ceebf0019a1f1db3b8462f2
i1 <ivansapiga1999@gmail.com>
2018-10-30 00:18:49 +0200

    [+]scroll in Detail

--- NulpAppApi/Controller/DetailViewController.swift
+++ NulpAppApi/Controller/DetailViewController.swift
@@ -13,9 +13,10 @@ class DetailViewController: UIViewController, UIGestureRecognizerDelegate {
     var follower: Follower?
     
     var imageZoomed = false
-    @IBOutlet weak var followerImage: UIImageView!
+
+    @IBOutlet weak var userImage: UIImageView!
     @IBOutlet weak var loginLabel: UILabel!
-    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var iDLabel: UILabel!
     @IBOutlet weak var nodeIDLabel: UILabel!
     @IBOutlet var tapGesture: UITapGestureRecognizer!
     
@@ -32,22 +33,22 @@ class DetailViewController: UIViewController, UIGestureRecognizerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         nodeIDLabel.text = "Node ID: \(follower!.node_id)"
-        idLabel.text = "ID: \(follower!.id)"
+        iDLabel.text = "ID: \(follower!.id)"
         loginLabel.text = "Login: \(follower!.login)"
-        followerImage.moa.url = "\(follower!.avatar_url)"
-        followerImage.isUserInteractionEnabled = true
+        userImage.moa.url = "\(follower!.avatar_url)"
+        userImage.isUserInteractionEnabled = true
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapImage))
         tapGesture.numberOfTapsRequired = 2
-        followerImage.addGestureRecognizer(tapGesture)
+        userImage.addGestureRecognizer(tapGesture)
     }
     
     func scaleImageUp() {
-        followerImage.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        userImage.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
         imageZoomed = true
     }
     
     func normalizeImage() {
-        followerImage.transform = CGAffineTransform.identity
+        userImage.transform = CGAffineTransform.identity
         imageZoomed = false
     }
 
